e4ee49be6681c679a43efccbb9c70cabd17d4b78
kewalaka <kewalaka@gmail.com>
2024-05-12 13:23:49 +1200

    fix: more lint fixes and bump versions

--- terraform.tf
+++ terraform.tf
@@ -1,21 +1,21 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.5.0"
   required_providers {
     azapi = {
       source  = "Azure/azapi"
-      version = ">= 1.9.0, < 2.0.0"
+      version = "~> 1.13"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.71.0, < 4.0.0"
+      version = "~> 3.100"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.5.0, < 4.0.0"
+      version = "~>= 3.5"
     }
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.9.1, < 2.0.0"
+      version = "~> 0.9.1"
     }
   }
 }
